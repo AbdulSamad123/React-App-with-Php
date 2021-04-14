3e5b2a5f4c40acd57903e42800e2d146c93c7277
Abdul Samad <Samad0182@gmail.com>
2021-04-14 20:22:19 +0500

    Add api

---
+++ insert.php
@@ -0,0 +1,25 @@
+<?php
+   $CN=mysqli_connect("localhost","root","");
+   $DB=mysqli_select_db($CN,"cst");
+
+   $RollNo = $_POST['RollNo'];
+   $StudentName = $_POST['StudentName'];
+   $Course = $_POST['Course'];
+
+   $IQ = "insert into studentmaster(RollNo,StudentName,Course) values($RollNo, '$StudentName', '$Course')";
+
+   $R=mysqli_query($CN,$IQ);
+
+   if($R)
+   {
+       $Message="Student has been registered successfully";
+   }
+   else
+   {
+       $Message="Server Error please try later";
+   }
+
+   $Response[]=array("Message"=>$Message);
+   echo json_encode($Message);
+
+?>
